1d86e29420151f3e1c23e879cb650de0e95a5aff
Rufxz <ricariorufus1602@gmail.com>
2023-05-22 00:15:53 +0200

    Initiating app_cont which consists of themes, languages, favourites, and history and also to delete all data.

---
+++ translate-master/lib/controller/app_controller.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:translate/model/model/languages_model.dart';
+
+import '../model/repository/get_info.dart';
+import '../model/store/local_store.dart';
+import '../model/model/translate_model.dart';
+
+class AppController extends ChangeNotifier{
+  List<TranslateModel> listOfFavourites=[];
+  List<TranslateModel> listOfHistory=[];
+  LanguagesModel lang=LanguagesModel();
+  bool isChangeTheme=false;
+  bool isLoading = true;
+  List<String?> languages = [];
+  List<String?> codes = [];
+
+
+  changeTheme(){
+    isChangeTheme=!isChangeTheme;
+    LocalStore.setTheme(isChangeTheme);
+    notifyListeners();
+  }
+
+  setTheme(bool theme){
+    isChangeTheme=theme;
+    notifyListeners();
+  }
+
+
+  getLang() async {
+    isLoading = true;
+    notifyListeners();
+
+    LanguagesModel? data = await LocalStore.getLanguages();
+    LanguagesModel? model;
+    if (data != null) {
+      model = data;
+    } else {
+      model = await GetInfo.getLanguages();
+      LocalStore.setLanguages(model!);
+    }
+    model.data?.languages?.forEach((element) {
+      languages.add(element.name);
+      codes.add(element.code);
+    });
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+
+
+  //-------------- Favourites ------------//
+
+  addFavourites(TranslateModel model){
+    listOfFavourites.add(model);
+    LocalStore.setFavourites(model);
+    notifyListeners();
+  }
+  removeFavourites(int index){
+    listOfFavourites.removeAt(index);
+    LocalStore.removeFavourites(index);
+    notifyListeners();
+  }
+  removeAllFavourites(){
+    listOfFavourites.clear();
+    LocalStore.removeAllFavourites();
+    notifyListeners();
+  }
+
+  //-------------- History ------------//
+
+  addHistory(TranslateModel model){
+    listOfHistory.add(model);
+    LocalStore.setHistory(model);
+    notifyListeners();
+  }
+  removeHistory(int index){
+    listOfHistory.removeAt(index);
+    LocalStore.removeHistory(index);
+    notifyListeners();
+  }
+  removeAllHistory(){
+    listOfHistory.clear();
+    LocalStore.removeAllHistory();
+    notifyListeners();
+  }
+
+  //-------------- Languages ------------//
+
+  addLanguages(LanguagesModel model){
+    lang=model;
+    LocalStore.setLanguages(model);
+    notifyListeners();
+  }
+//-------------- All ------------//
+  clearAllData(){
+    listOfHistory.clear();
+    listOfFavourites.clear();
+    LocalStore.clearAllData();
+    notifyListeners();
+  }
+
+}
+
